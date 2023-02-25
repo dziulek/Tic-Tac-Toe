1880980509c757fb4268eec1eba9bca803d16731
Piotr Kaminski <99peterstone@gmail.com>
2023-02-25 23:33:44 +0100

    feat: init of testing game infrastr.

---
+++ test/test_game.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+import sys
+
+from src.game import TicTacGame
+
+class makeMoveTest(unittest.TestCase):
+
+    def test_game_make_move(self):
+
+        game_1 = TicTacGame(1)
+        
+
+        game_2 = TicTacGame(2)
+
+        while ~game_1.termninal():
+            
+            game_1.make_move()
